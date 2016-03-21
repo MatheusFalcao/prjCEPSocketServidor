24a6e560f9aaeeb1cb4c84c552bebad5431581cf
Matheus Falcão <maatheusfalcao@gmail.com>
2016-03-21 19:42:34 -0300

    Commit Inicial
    Commit Inicial

---
+++ src/modelo/Endereco.java
@@ -0,0 +1,16 @@
+package modelo;
+
+public class Endereco {
+	private String cep;
+	private String rua;
+	private String bairro;
+	private String cidade;
+	
+	
+	public String consultaCep(String cep){
+		this.cep = cep;
+		
+		return null;
+	}
+
+}
